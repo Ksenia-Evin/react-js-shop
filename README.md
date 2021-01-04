4371c998c588df02a6102103464816f48cab0419
Ksenia-Evin <ksu_ryaz@mail.ru>
2021-01-04 15:19:41 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React On-line shop
---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+function App() {
+  return (
+    <div className="container"> 
+    <header> 
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li>Shop</li>
+        <li>Contacts</li>
+        <li>Delivery</li>
+        <li>Basket</li>
+      </ul>    
+    </header>
+    <main> 
+      Welcome to the online Shopping Center!
+    </main>
+    <footer> 
+      All right is reserved &copy;
+    </footer>
+    </div>
+  );
+}
+
+export default App;
